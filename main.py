9995f2da382e617407e04faded2da0e288ac2a24
Katie Dittrich <katieanna.dittrich@gmail.com>
2021-03-07 16:52:08 +0100

    Added lists and dictionaries and created external function

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
+from summen import gesamtsumme
+
 name = input('Bitte gib Deinen Namen ein: ')
 print('Hello, ' + name + '!\n')
+
+fahrzeug1 = {"marke": "vw", "modell": "golf", "baujahr": 2012, "preis": 5000}
+fahrzeug2 = {"marke": "renault", "modell": "clio", "baujahr": 2011, "preis": 6000}
+fahrzeuge = [fahrzeug1, fahrzeug2]
+
+gesamtpreis = 0
+for fahrzeug in fahrzeuge:
+    print(fahrzeug["preis"])
+    gesamtpreis = gesamtpreis + (fahrzeug["preis"])
+print('Der Gesamtpreis aller Fahrzeuge ist', gesamtpreis, '€')
+
+print(gesamtsumme())
+print(gesamtsumme(10))
+print(gesamtsumme(100))
 
 zahl = eval(input('Bitte gib eine Zahl größer als 100 zum Verdoppeln ein: '))
 
---
+++ summen.py
@@ -0,0 +1,5 @@
+def gesamtsumme(max_zahl=1):
+    j = 0
+    for i in range(1, max_zahl + 1):
+        j = j + i
+    return j
